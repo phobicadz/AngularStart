11975810e4325ad7062c06a2de5017a3a72f4f43
phobicadz <chamberlain_adam@hotmail.com>
2015-07-24 19:51:56 +0100

    .

---
+++ Backup/AngularStart1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularStart1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Angular()
+        {
+            return View();
+        }
+
+        public ActionResult Demo()
+        {
+            return View();
+        }
+
+        public ActionResult Transaction()
+        {
+            return View();
+        }
+    }
+}

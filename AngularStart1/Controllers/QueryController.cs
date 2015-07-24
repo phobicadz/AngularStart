b561187e3a64a3fee47794e96a278326fdec983f
phobicadz <chamberlain_adam@hotmail.com>
2015-07-24 20:09:22 +0100

    .

--- AngularStart1/Controllers/QueryController.cs
+++ AngularStart1/Controllers/QueryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Omg.CustomerSupport;
+//using Omg.CustomerSupport;
 
 namespace AngularStart1.Controllers
 {
